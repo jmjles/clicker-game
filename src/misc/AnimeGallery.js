dd869cb97f87e446b0f2e71c072ae478f63c4f5f
jmjles <jmjles@gmail.com>
2020-02-17 10:03:06 -0800

    Removed Redundant Code

--- src/misc/AnimeGallery.js
+++ src/misc/AnimeGallery.js
@@ -1,10 +1,8 @@
 import anime from "animejs/lib/anime";
 
 export const openGameInfo = open => {
-  const windowWidth = document.getElementsByClassName("GameInfo")[0]
-    .clientWidth;
   let width = [];
-  open ? (width = [windowWidth, 1300]) : (width = [windowWidth, 0]);
+  open ? (width = [0, '100vw']) : (width = ['100vw', 0]);
   anime({
     targets: ".GameInfo",
     width,
@@ -14,14 +12,12 @@ export const openGameInfo = open => {
 };
 
 export const openCharacterSelection = open => {
-  const windowWidth = document.getElementsByClassName("CharacterSelect")[0]
-    .clientWidth;
   let width = [];
-  open ? (width = [windowWidth, 1300]) : (width = [windowWidth, 0]);
+  open ? (width = [0, '100vw']) : (width = ['100vw', 0]);
   anime({
     targets: ".CharacterSelect",
     width,
     easing: "linear",
-    duration: 1000
+    duration: 300
   });
 };
